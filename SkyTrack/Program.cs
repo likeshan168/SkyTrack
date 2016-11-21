1cc3996323c8e77ecd619d0f32c66175d5ae0172
likeshan168 <likeshan168@163.com>
2016-11-21 12:31:04 +0800

    加入顺丰的测试代码

--- SkyTrack/Program.cs
+++ SkyTrack/Program.cs
@@ -1,11 +1,15 @@
 ﻿
 using SkyTrack.Bussiness;
+using SkyTrack.Common;
 using SkyTrack.ru.dpd.ws2;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace SkyTrack
 {
@@ -27,16 +31,36 @@ namespace SkyTrack
 
                 #region tracking service
 
-                ParcelTracingService trackingService = new ParcelTracingService();
-                //使用mockservice的地址可以获测试数据
-                trackingService.Url = "http://desktop-avssgiq:8080/MockService";
-                auth au = new auth() { clientKey = clientKey, clientNumber = clientNo };
-                stateParcels states = null;
-                if (GetStateParcels(trackingService, au, out states) && states != null)
-                {
-                    Console.WriteLine("更新数据库成功");
-                    Confirm(trackingService, au, states.docId);
-                }
+                //ParcelTracingService trackingService = new ParcelTracingService();
+                ////使用mockservice的地址可以获测试数据
+                //trackingService.Url = "http://desktop-avssgiq:8080/MockService";
+                //auth au = new auth() { clientKey = clientKey, clientNumber = clientNo };
+                //stateParcels states = null;
+                //if (GetStateParcels(trackingService, au, out states) && states != null)
+                //{
+                //    Console.WriteLine("更新数据库成功");
+                //    Confirm(trackingService, au, states.docId);
+                //}
+
+                #endregion
+
+                #region sf web service
+                //XmlDocument xmlDoc = new XmlDocument();
+                //xmlDoc.Load(AppDomain.CurrentDomain.BaseDirectory + "XML/RequestBody.xml");
+                //string xml = xmlDoc.InnerXml;
+                //string checkWorld = "j8DzkIFgmlomPt0aLuwU";
+                ////MD5
+                //string verifyCode = Encrypt.Instance.MD5Encrypt($"{xml}{checkWorld}");
+                //string param = $"xml={xml}&verifyCode={verifyCode}";
+                //string result = PostWebRequest("http://bspoisp.sit.sf-express.com:11080/bsp-oisp/sfexpressService", param, Encoding.UTF8);
+
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(AppDomain.CurrentDomain.BaseDirectory + "XML/RequestBody.xml");
+                string xml = xmlDoc.InnerXml;
+                string checkWorld = "j8DzkIFgmlomPt0aLuwU";
+                string verifyCode = Encrypt.Instance.MD5Encrypt($"{xml}{checkWorld}");
+                SFService.ExpressServiceClient client = new SFService.ExpressServiceClient();
+                string result = client.sfexpressService(xml, verifyCode);
 
                 #endregion
 
@@ -82,6 +106,35 @@ namespace SkyTrack
                 Console.WriteLine(ex.Message);
                 return false;
             }
+        }
+
+
+        private static string PostWebRequest(string postUrl, string paramData, Encoding dataEncode)
+        {
+            string ret = string.Empty;
+            try
+            {
+                byte[] byteArray = dataEncode.GetBytes(paramData); //转化
+                HttpWebRequest webReq = (HttpWebRequest)WebRequest.Create(new Uri(postUrl));
+                webReq.Method = "POST";
+                webReq.ContentType = "application/x-www-form-urlencoded";
+
+                webReq.ContentLength = byteArray.Length;
+                Stream newStream = webReq.GetRequestStream();
+                newStream.Write(byteArray, 0, byteArray.Length);//写入参数
+                newStream.Close();
+                HttpWebResponse response = (HttpWebResponse)webReq.GetResponse();
+                StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                ret = sr.ReadToEnd();
+                sr.Close();
+                response.Close();
+                newStream.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return ret;
         }
     }
 }
